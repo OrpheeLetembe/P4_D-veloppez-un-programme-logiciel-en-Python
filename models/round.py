0293ee24a812ca0f477be883310e1d0b73b60fd4
Orphée <letembeorphee@gmail.com>
2021-07-18 23:17:15 +0200

    round counter

--- models/round.py
+++ models/round.py
@@ -1,13 +1,30 @@
 
+from typing import List
+import datetime
+
+
 class Round:
 	"""bla"""
+	number_of_rounds = 1
+
 	def __init__(self, name):
 		self.name = name
 		self.list_match = []
+		self.start_date = datetime.datetime.now()
+		self.end_date = ""
+
+		Round.number_of_rounds += 1
+
+	def add_match(self, match):
+		self.list_match.append(match)
+
+	def save_date(self):
+		self.start_date = datetime.datetime.now()
 
 
 class Match:
 	"""bla"""
-
 	def __init__(self):
-		pass
+		self.mane = ()
+
+
