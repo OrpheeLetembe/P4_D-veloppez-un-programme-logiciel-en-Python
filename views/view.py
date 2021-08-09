83d651d18f2c577b0b09376b15ae67113b6c8192
Orphée <letembeorphee@gmail.com>
2021-08-09 22:43:55 +0200

    Add function to display list players

--- views/view.py
+++ views/view.py
@@ -2,12 +2,14 @@
 class View:
     pass
 
-    def show_main_menu(self):
+    @staticmethod
+    def show_main_menu():
         print()
         print("        --Main menu--")
         print(" Enter 1 to create a new tournament")
         print(" Enter 2 to display the tournament list")
-        print(" Enter 3 to exit")
+        print(" Enter 3 to display the player list")
+        print(" Enter 4 to exit")
         print()
 
         response = input(" Your choice :")
@@ -16,13 +18,14 @@ class View:
             return None
         return response
 
-    def show_secondary_menu(self):
+    @staticmethod
+    def show_secondary_menu():
         print()
         print("      --Tournament menu--")
         print(" Enter 1 to add the results of round")
-        print(" Enter 2 to update the ranking")
-        print(" Enter 3 to display the list of players")
-        print(" Enter 4 to add comment")
+        print(" Enter 2 to display the list of players")
+        print(" Enter 3 to add comment")
+        print(" Enter 4 to save tournament")
         print(" Enter 5 to go to main menu")
         print()
         choice = input(" select a menu item: ")
@@ -31,10 +34,29 @@ class View:
             return None
         return choice
 
-    def show_message(self, message):
+    @staticmethod
+    def prompt_user_input(text):
+        user_input = input(text + ": ")
+        if not user_input:
+            return None
+        return user_input
+
+    @staticmethod
+    def show_message(message):
         print(message)
 
-    def prompt_tournament_name(self):
+    @staticmethod
+    def show_players(list_players):
+        for player in list_players:
+
+            print("Last name: {}".format(player.last_name))
+            print("First name: {}".format(player.first_name))
+            print("Date of birth: {}".format(player.date_of_birth))
+            print("Ranking: {}".format(player.ranking))
+            print("_____________________________________")
+
+    @staticmethod
+    def prompt_tournament_name():
         """Prompt for a name."""
         print("______Tournament information______")
         name = input("Name: ")
@@ -43,7 +65,8 @@ class View:
             return None
         return name
 
-    def prompt_tournament_location(self):
+    @staticmethod
+    def prompt_tournament_location():
         """Prompt for a location."""
         location = input("Location: ")
 
@@ -51,7 +74,8 @@ class View:
             return None
         return location
 
-    def prompt_tournament_date(self):
+    @staticmethod
+    def prompt_tournament_date():
         """Prompt for a date."""
         date = input("Date (day/month/year): ")
 
@@ -59,7 +83,8 @@ class View:
             return None
         return date
 
-    def prompt_tournament_pace(self):
+    @staticmethod
+    def prompt_tournament_pace():
         """ prompt for tournament pace"""
         pace = input("Time control (Enter 1 for bullet, 2 for blitz or 3 for quick hit): ")
 
@@ -67,21 +92,25 @@ class View:
             return None
         return pace
 
-    def prompt_tournament_comment(self):
+    @staticmethod
+    def prompt_tournament_comment():
         comment = input("Add a comment: ")
 
         if not comment:
             return None
         return comment
 
-    def show_players_title(self):
+    @staticmethod
+    def show_players_title():
         print("________ Player registration _______")
         print()
 
-    def show_player_number(self):
-        print("    ------Player {} ------".format(str(self)))
+    @staticmethod
+    def show_player_number(number):
+        print("    ------Player {} ------".format(str(number)))
 
-    def prompt_player_last_name(self):
+    @staticmethod
+    def prompt_player_last_name():
         """Prompt for a last name."""
         last_name = input("Last name: ")
 
@@ -89,15 +118,16 @@ class View:
             return None
         return last_name
 
-    def prompt_player_first_name(self):
+    @staticmethod
+    def prompt_player_first_name():
         """Prompt for a first name."""
         first_name = input("First name: ")
 
         if not first_name:
             return None
         return first_name
-
-    def prompt_player_date_of_birth(self):
+    @staticmethod
+    def prompt_player_date_of_birth():
         """Prompt for a date of birth."""
         date_of_birth = input("Date of birth (day/month/year) : ")
 
@@ -105,7 +135,8 @@ class View:
             return None
         return date_of_birth
 
-    def prompt_player_gender(self):
+    @staticmethod
+    def prompt_player_gender():
         """Prompt for a gender."""
         gender = input("Gender (M/F): ").capitalize()
 
@@ -113,7 +144,8 @@ class View:
             return None
         return gender
 
-    def prompt_player_ranking(self):
+    @staticmethod
+    def prompt_player_ranking():
         """Prompt for a ranking."""
         ranking = input("Ranking: ")
 
@@ -121,10 +153,12 @@ class View:
             return None
         return ranking
 
-    def show_opponent(self, k, player1, player2):
+    @staticmethod
+    def show_opponent(k, player1, player2):
         return print("Match {}: {} VS {}".format(k + 1, player1, player2))
 
-    def prompt_of_winner(self, player1, player2):
+    @staticmethod
+    def prompt_of_winner(player1, player2):
 
         winner = input("Winner (Enter 1 for {} , 2 for {} or 3 for draw) : ".format(player1, player2))
 
@@ -132,9 +166,21 @@ class View:
             return None
         return winner
 
-    def show_tournament_list(self, list_tournament):
+    @staticmethod
+    def show_tournament_list(list_tournament):
         for k, tournament in enumerate(list_tournament):
             print(k + 1, tournament)
 
-    def show_update_kanking(self, player1, player2):
-        print("{}, current ranking: {}".format(player1, player2))
+    @staticmethod
+    def show_update_ranking(list_players):
+        for player in list_players:
+            print(list_players.index(player) + 1, player.last_name, player.first_name)
+
+        selected_player = input("Select a player id: ")
+
+        if not selected_player:
+            return None
+        return selected_player
+
+
+
