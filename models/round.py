4f75564752bd309b246ca67196bdec44ec61012c
Orphée <letembeorphee@gmail.com>
2021-08-09 19:57:59 +0200

    Add fix the error

--- models/round.py
+++ models/round.py
@@ -1,5 +1,3 @@
-
-
 import datetime
 
 
@@ -7,7 +5,6 @@ class Round:
     """
     A class to represent a round.
         ...
-
     Attributes
     ----------
     name : str
@@ -18,7 +15,6 @@ class Round:
         starting date of the round
     end_date   : str
         end date of the round
-
     Methods
     -------
     add_match(match):
@@ -26,13 +22,11 @@ class Round:
     save_end_date():
         saves the date and time of the end of the round
     lose():
-
     """
 
-    def __init__(self, name):
+    def __init__(self, name, **kwargs):
         """
         adds a match to the match list of a round
-
         :param name: list
         """
         self.name = name
@@ -41,9 +35,20 @@ class Round:
         self.start_date = datetime.datetime.now()
         self.end_date = ""
 
+        if "list_match" in kwargs:
+            self.list_match = kwargs["list_match"]
+
+        if "list_match_score" in kwargs:
+            self.list_match_score = kwargs["list_match_score"]
+
+        if "star_date" in kwargs:
+            self.start_date = kwargs["start_date"]
+
+        if "end_date" in kwargs:
+            self.end_date = kwargs["end_date"]
+
     def add_match(self, match):
         """
-
         :param match:
         :return:
         """
@@ -62,24 +67,38 @@ class Round:
     def __repr__(self):
         return repr(self.name)
 
+    def serialize(self):
+        return {
+            "name": self.name,
+            "opponent": [x.serialise() for y in self.list_match for z in y for x in z],
+            "match": [x.serialise() for x in self.list_match_score],
+            "start": self.start_date,
+            "end": self.end_date
+        }
+
+    @classmethod
+    def deserialize(cls, serialize_round: dict) -> "Round":
+
+        return Round(
+            name=serialize_round.get("name"),
+            list_match=serialize_round.get("opponent"),
+            list_match_score=serialize_round.get("match"),
+            start_date=serialize_round.get("start"),
+            end_date=serialize_round.get("end")
+
+        )
+
 
 class Match:
     """
     A class to represent a match.
-
     Attributes
     ----------
     list1 : list
-
-
-
-
-
     """
 
     def __init__(self, list1, list2):
         """
-
         :param list1:
         :param list2:
         """
@@ -87,3 +106,9 @@ class Match:
 
     def __repr__(self):
         return repr(self.name)
+
+    def serialize(self):
+        return {
+
+            "match": self.name
+        }
