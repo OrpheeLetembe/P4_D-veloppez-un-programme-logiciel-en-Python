74c6192140c771703bcdf7ab1c8495b8b1503011
Orphée <letembeorphee@gmail.com>
2021-08-09 19:57:46 +0200

    Add fix the error

--- models/player.py
+++ models/player.py
@@ -1,10 +1,7 @@
-
-
 class Player:
     """
     A class to represent a player.
     ...
-
     Attributes
     ----------
     last_name : str
@@ -19,7 +16,6 @@ class Player:
         player's ranking
     score     : int
         player's score
-
     Methods
     -------
     Win():
@@ -27,13 +23,11 @@ class Player:
     draw():
         Adds 0.5 to player's score
     lose():
-
     """
 
-    def __init__(self, last_name, first_name, birth, gender, ranking):
+    def __init__(self, last_name, first_name, birth, gender, ranking, score=None):
         """
         Constructs all the necessary attributes for the player object.
-
         :param last_name: str
             family name of the player
         :param first_name: str
@@ -45,16 +39,19 @@ class Player:
         :param ranking: int
             ranking of the player
         """
-
+        if score is None:
+            score = 0
         self.last_name = last_name
         self.first_name = first_name
         self.date_of_birth = birth
         self.gender = gender
         self.ranking = ranking
-        self.score = 0
+        self.score = score
+
+
 
     def __repr__(self):
-        return repr((self.last_name, self.first_name, self.date_of_birth, self.gender, self.ranking, self.score))
+        return repr((self.last_name, self.first_name, self.date_of_birth, self.gender, self.ranking))
 
     def win(self):
         """ This function allows you to add  1 to the player's score.
@@ -73,6 +70,34 @@ class Player:
         :return: None
         """
         self.score += 0
+
+    def serialize(self):
+        """
+        :return:
+        """
+        return {
+            "last_name": self.last_name,
+            "first_name": self.first_name,
+            "birth": self.date_of_birth,
+            "gender": self.gender,
+            "ranking": self.ranking,
+            "score": self.score
+        }
+
+    @classmethod
+    def deserialize(cls, serialized_player: dict) -> "Player":
+        """
+        :param serialized_player:
+        :return:
+        """
+        return Player(
+            last_name=serialized_player.get("last_name"),
+            first_name=serialized_player.get("first_name"),
+            birth=serialized_player.get("birth"),
+            gender=serialized_player.get("gender"),
+            ranking=serialized_player.get("ranking"),
+            score=serialized_player.get("score"),
+        )
 
 
 def main():
