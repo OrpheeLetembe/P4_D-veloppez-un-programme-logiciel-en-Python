ae2cf39189c10bacadc094d29b3bb61b98c8a593
Orphée <letembeorphee@gmail.com>
2021-08-10 23:51:32 +0200

    fix flake8 error

--- views/view.py
+++ views/view.py
@@ -4,12 +4,16 @@ class View:
 
     @staticmethod
     def show_main_menu():
+        """
+
+        :return:
+        """
         print()
         print("        --Main menu--")
         print(" Enter 1 to create a new tournament")
         print(" Enter 2 to display the tournament list")
         print(" Enter 3 to display the player list")
-        print(" Enter 4 to exit")
+        print(" Enter 4 to exit main menu")
         print()
 
         response = input(" Your choice :")
@@ -20,6 +24,10 @@ class View:
 
     @staticmethod
     def show_secondary_menu():
+        """
+
+        :return:
+        """
         print()
         print("      --Tournament menu--")
         print(" Enter 1 to add the results of round")
@@ -36,6 +44,11 @@ class View:
 
     @staticmethod
     def prompt_user_input(text):
+        """
+
+        :param text:
+        :return:
+        """
         user_input = input(text + ": ")
         if not user_input:
             return None
@@ -43,10 +56,20 @@ class View:
 
     @staticmethod
     def show_message(message):
+        """
+
+        :param message:
+        :return:
+        """
         print(message)
 
     @staticmethod
     def show_players(list_players):
+        """
+
+        :param list_players:
+        :return:
+        """
         for player in list_players:
 
             print("Last name: {}".format(player.last_name))
@@ -57,15 +80,32 @@ class View:
 
     @staticmethod
     def show_player_number(number):
+        """
+
+        :param number:
+        :return:
+        """
         print("    ------Player {} ------".format(str(number)))
 
     @staticmethod
     def show_opponent(k, player1, player2):
+        """
+
+        :param k:
+        :param player1:
+        :param player2:
+        :return:
+        """
         return print("Match {}: {} VS {}".format(k + 1, player1, player2))
 
     @staticmethod
     def prompt_of_winner(player1, player2):
+        """
 
+        :param player1:
+        :param player2:
+        :return:
+        """
         winner = input("Winner (Enter 1 for {} , 2 for {} or 3 for draw) : ".format(player1, player2))
 
         if not winner:
@@ -74,11 +114,21 @@ class View:
 
     @staticmethod
     def show_tournament_list(list_tournament):
+        """
+
+        :param list_tournament:
+        :return:
+        """
         for k, tournament in enumerate(list_tournament):
-            print(k + 1, tournament.name, tournament.location, tournament.date)
+            print("{} - {}, {} {} Note: {}".format(k + 1, tournament.name, tournament.location, tournament.date,
+                                                   tournament.description))
 
     @staticmethod
     def show_selected_tournament():
+        """
+
+        :return:
+        """
         print("________Tournament menu________")
         print("1 Display players list")
         print("2 Display rounds")
@@ -91,9 +141,14 @@ class View:
 
     @staticmethod
     def show_round(list_rounds):
+        """
+
+        :param list_rounds:
+        :return:
+        """
         print("_________________")
         for r in list_rounds:
-            print(r.name)
+            print("{}, start date:{} - end date:{}".format(r.name, r.start_date, r.end_date))
 
             for m in r.list_match_score:
                 m1 = m.get("match")
@@ -103,6 +158,11 @@ class View:
 
     @staticmethod
     def show_update_ranking(list_players):
+        """
+
+        :param list_players:
+        :return:
+        """
         for player in list_players:
             print(list_players.index(player) + 1, player.last_name, player.first_name)
 
@@ -111,6 +171,3 @@ class View:
         if not selected_player:
             return None
         return selected_player
-
-
-
